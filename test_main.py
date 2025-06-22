9b5b2827ee7e670a026876db29d0ecde56969c3f
barsha <barsha59@barsha.barsha.com>
2025-06-22 15:11:11 +0545

    Add working test for add() fucntion

--- test_main.py
+++ test_main.py
@@ -1,4 +1,8 @@
+# test_main.py
+
 from main import add
 
 def test_add():
-    assert add(2, 3) == 5
+    assert add(2, 3) == 5         # simple case
+    assert add(-1, 1) == 0        # edge case (positive + negative)
+    assert add(0, 0) == 0         # zero case
